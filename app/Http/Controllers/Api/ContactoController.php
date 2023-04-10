a692b8ba12de5d139baa1bffc747fd5414d44520
Cecilia Ceballos <ceciliaceb@gmail.com>
2023-04-09 21:45:39 -0300

    version 1 - API Front

---
+++ app/Http/Controllers/Api/ContactoController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Exception;
+use App\Models\Contacto;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Validated;
+//reglas
+use Illuminate\Support\Facades\Validator;
+
+class ContactoController extends Controller
+{
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+        ////// Reglas
+            
+            $validator = Validator::make($request->all(), [
+                
+                'nombre'=>'required|min:2|max:25',
+                'apellido'=>'required|min:2|max:25',
+                'telefono'=>'required|regex:/^([0-9\s\-\+\(\)]*)$/|between:5,15',
+                'email' => 'required|max:30|email:rfc,dns|unique:contactos,con_email',
+                
+            ]);
+
+            if( $validator->fails() ) {
+                return response()->json([
+                    'success'   =>  false,
+                    'message'   =>  $validator->errors(),
+                    'rdo'       => 1
+                ], 200);
+            }
+
+            //
+            $contacto = new Contacto();
+            $contacto->con_nombre =$request->nombre;
+            $contacto->con_apellido =$request->apellido;
+            $contacto->con_telefono =$request->telefono;
+            $contacto->con_email =$request->email;
+            $contacto->con_descripcion =$request->descripcion;
+                
+            $contacto->save();
+            
+            return response()->json([
+                'mensaje' => 'Registramos Exitosamente tus datos ' .$request->nombre,
+                //'mensaje' => 'Registramos Exitosamente tus datos ', 
+                'rdo'=>0,
+            ]);
+       
+    }
+}
